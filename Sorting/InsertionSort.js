0b0e61d01869cb24bd5e49b085e09211338506ab
karolsw2 <karol.sw3@gmail.com>
2018-06-02 14:24:28 +0200

    Add the insertion sort algorithm

---
+++ Sorting/InsertionSort.js
@@ -0,0 +1,19 @@
+/*
+* The insertion sort algorithm
+* Time complexity:
+*   Best case: O(n)
+*   Worst case: O(n^2)
+*   Average: O(n^2)
+* Space complexity: O(n)
+*/
+
+function insertionSort (array) {
+  for (let i = 0; i < array.length; i++) {
+    let temp = array[i]
+    for (var j = i - 1; j > -1 && array[j] > temp; j--) {
+      array[j + 1] = array[j]
+    }
+    array[j + 1] = temp
+  }
+  return array
+}
