f52df868b1c0a6201aa2c6a361ddf6658cc12408
karolsw2 <karol.sw3@gmail.com>
2018-06-11 19:44:00 +0200

    Add c++ implementation of the bubble sort algorithm

---
+++ Sorting/Comparision sorts/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void bubbleSort(int array[], int n) {
+	for (int cycle = 0; cycle < n - 1; cycle++) {
+		for (int pointer = 0; pointer < n - cycle - 1; pointer++) {
+			if(array[pointer] > array[pointer + 1]) {
+				std::swap(array[pointer], array[pointer + 1]);
+			}
+		}
+	}
+}
