da4e49ef3a3f4c1bea76f1ae360ea5ea9712dbd1
karolsw2 <karol.sw3@gmail.com>
2018-06-17 10:46:23 +0200

    Implement Stack data structure in C++

---
+++ Data structures/Stack/Stack.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+// I know this is trivial, but I had a lot of fun while coding this.
+template<typename T>
+class Stack {
+	public:
+		Stack() {
+			stack.clear();
+		};
+		void push(T value);
+		T pop();
+		T getTop();
+	private:
+		std::vector<T> stack;
+};
+
+template<typename T>
+void Stack<T>::push (T value) {
+	this->stack.push_back(value);
+}
+
+template<typename T>
+T Stack<T>::pop () {
+	T back = this->stack.back();
+	this->stack.pop_back();
+	return back;
+}
+
+template<typename T>
+T Stack<T>::getTop () {
+	size_t back = this->stack.size() - 1;
+	return this->stack.at(back);
+}
+
+int main () {
+	Stack<int> stack;
+	stack.push(3);
+	std::cout << stack.pop();
+	return 0;
+}
