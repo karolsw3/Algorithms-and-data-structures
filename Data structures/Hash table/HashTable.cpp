634c1ea8f608d6fadd821ac4fa18f9c28669ade9
karolsw2 <karol.sw3@gmail.com>
2018-06-21 19:37:13 +0200

    Add function to print the HashTable data

--- Data structures/Hash table/HashTable.cpp
+++ Data structures/Hash table/HashTable.cpp
@@ -6,7 +6,7 @@ class Person {
   public:
     Person () {
       // You wouldn't talk with someone like this, would you?
-      name = "";
+      name = "null";
       age = 0;
     }
     Person (std::string newName, unsigned int newAge) {
@@ -39,6 +39,7 @@ class LinkedList {
     Node<T>* head;
     LinkedList () {
       head = NULL;
+      size = 0;
     }
     void push (T* data) {
       Node<T>* node = new Node<T>();
@@ -54,11 +55,14 @@ class LinkedList {
     size_t size;
 };
 
-template <typename T>
 class HashTable {
   public:
     HashTable (size_t size) : vect(size) {
-      arraySize = size;
+      vectSize = size;
+    }
+
+    size_t getSize () {
+      return vectSize;
     }
 
     void addPerson (Person* person) {
@@ -66,7 +70,7 @@ class HashTable {
     }
 
     Person* getPerson (std::string name) {
-      Node<T>* currentNode = vect[hash(name)].head;
+      Node<Person>* currentNode = vect[hash(name)].head;
       for (size_t i = 0; i < vect[hash(name)].getSize(); i++) {
         if (currentNode->data->getName() == name) {
           return currentNode->data;
@@ -74,26 +78,38 @@ class HashTable {
         currentNode = currentNode->next;
       }
     }
+
+    void print () {
+      for (size_t i = 0; i < vectSize - 1; i++) {
+        std::cout << "Index " << i << ": ";
+        Node<Person>* currentNode = vect[i].head;
+        for (size_t j = 0; j < vect[i].getSize(); j++) {
+          Person* person = currentNode->data;
+          std::cout << person->getName() << " ";
+          currentNode = currentNode->next;
+        }
+        std::cout << std::endl;
+      }
+    }
   private:
     std::vector<LinkedList<Person>> vect;
-    size_t arraySize;
+    size_t vectSize;
 
     size_t hash (std::string name) {
       size_t id = 0; 
       for (char c : name) {
         id += size_t(c);
       }
-      return id % arraySize;
+      return id % vectSize;
     }
 };
 
 int main () {
-  HashTable<Person>* table = new HashTable<Person>(10);
-  Person* ola = new Person("Ola", 12);
-  table->addPerson(ola);
+  HashTable* table = new HashTable(4);
   table->addPerson(new Person("Aga", 19));
   table->addPerson(new Person("Jacek", 23));
   table->addPerson(new Person("Michał", 67));
   table->addPerson(new Person("Baśka", 10));
-  std::cout << table->getPerson("Baśka")->getAge() << std::endl;
+  table->print();
+  return 1;
 }
